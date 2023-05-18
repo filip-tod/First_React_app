ace389cd5114f9f6ac16d8250dec4926622c5536
filip-tod <filip.tod95@gmail.com>
2023-05-18 20:26:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Prva <code>React</code> aplikacija
         </p>
         <a
           className="App-link"
